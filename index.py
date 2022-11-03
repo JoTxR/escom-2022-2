9662a6e6b2d10bbae9bec25f89bcd3d98ec1ddfa
JoTxR <106110584+JoTxR@users.noreply.github.com>
2022-11-03 12:57:45 -0600

    update

---
+++ index.py
@@ -0,0 +1,13 @@
+from turtle import *
+color('red')
+N = 0
+while(N!=10):
+    begin_fill()
+    pensize(3)
+    left(50)
+    forward(133)
+    circle(50,200)
+    right(140)
+    circle(50,200)
+    forward(133)
+    N+=1
