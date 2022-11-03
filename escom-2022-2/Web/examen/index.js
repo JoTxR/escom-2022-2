153133165fc1fc4444d2a80ad487164a7ab239d8
JoTxR <106110584+JoTxR@users.noreply.github.com>
2022-11-03 12:43:37 -0600

    actualizacion examen 03/11

--- escom-2022-2/Web/examen/index.js
+++ escom-2022-2/Web/examen/index.js
@@ -2,12 +2,11 @@ function aleatorio(max){
    return Math.floor(Math.random() * max)
 }
 var n=0,num;
-var x1 =100,y1=200;
-var x2=300,y2=200;
-var x3 =200,y3=100;
-var p1=300,p2=300;
-var pmx=0,pmy=0;
-var color='aqua',numero = 0;
+var x1 =0,y1=346;
+var x2=200,y2=0;
+var x3 =400,y3=346;
+var p1=0,p2=0;
+var color='black',numero = 0;
 color=window.prompt('inserte un color en ingles:'); 
 while(n < 1000){
 n=window.prompt('inserte un numero mayor a 1000:');  
@@ -19,7 +18,7 @@ var contexto = canvas.getContext("2d");
     contexto.fillRect(x2,y2,2,2);
     contexto.fillRect(x3,y3,2,2);
     contexto.fillRect(p1,p2,2,2);
-    
+     
 for(var i = 0 ; i <= n ; i++){
    num = aleatorio(2);
    if (num == 0){
